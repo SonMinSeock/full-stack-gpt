0691b01e118a98ba757f84509ad7c71628e21608
손민석 <zonins3@gmail.com>
2026-09-04 18:19:41 +0900

    chore: GPT-4 Turbo

--- pages/QuizGPT.py
+++ pages/QuizGPT.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from langchain.retrievers import WikipediaRetriever
-from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import UnstructuredFileLoader
+from langchain.chat_models import ChatOpenAI
 
 st.set_page_config(
     page_title="QuizGPT",
@@ -10,6 +10,11 @@ st.set_page_config(
 )
 
 st.title("QuizGPT")
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    model="gpt-3.5-turbo-1106",
+)
 
 @st.cache_resource(show_spinner="Loading file...")
 def split_file(file):
@@ -31,6 +36,7 @@ def split_file(file):
         return docs
 
 with st.sidebar:
+    docs = None
     choice = st.selectbox(
         "Choose what you want to use.",
         ("File", "Wikipedia Article"),
@@ -43,11 +49,23 @@ with st.sidebar:
         )
         if file:
             docs = split_file(file)
-            st.write(docs)
 
     else:
         topic = st.text_input("Search Wikipedia...")
         if topic:
             retriever = WikipediaRetriever(top_k_results=5)
             with st.status("Searching Wikipedia..."):
-                docs = retriever.get_relevant_documents(topic)        
+                docs = retriever.get_relevant_documents(topic)
+
+if not docs:
+    st.markdown(
+        """
+    Welcome to QuizGPT.
+                
+    I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
+                
+    Get started by uploading a file or searching on Wikipedia in the sidebar.
+    """
+    )
+else:
+    st.write(docs)
