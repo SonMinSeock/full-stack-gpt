c601e11849b25f3e8c11165557ae117ee1d93254
손민석 <zonins3@gmail.com>
2026-09-08 08:25:58 +0900

    chore: Map Re Rank Chain

--- pages/SiteGPT.py
+++ pages/SiteGPT.py
@@ -1,12 +1,61 @@
 from langchain.document_loaders import SitemapLoader
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from bs4 import BeautifulSoup
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
 
-def parse_page(soup: BeautifulSoup):
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+answers_prompt = ChatPromptTemplate.from_template(
+    """
+    Using ONLY the following context answer the user's question. If you can't just say you don't know, don't make anything up.
+                                                  
+    Then, give a score to the answer between 0 and 5.
+
+    If the answer answers the user question the score should be high, else it should be low.
+
+    Make sure to always include the answer's score even if it's 0.
+
+    Context: {context}
+                                                  
+    Examples:
+                                                  
+    Question: How far away is the moon?
+    Answer: The moon is 384,400 km away.
+    Score: 5
+                                                  
+    Question: How far away is the sun?
+    Answer: I don't know
+    Score: 0
+                                                  
+    Your turn!
+
+    Question: {question}
+"""
+)
+
+
+def get_answers(inputs):
+    docs = inputs["docs"]
+    question = inputs["question"]
+    answers_chain = answers_prompt | llm
+    answers = []
+    for doc in docs:
+        result = answers_chain.invoke(
+            {"question": question, "context": doc.page_content}
+        )
+        answers.append(result.content)
+    st.write(answers)
+
+
+def parse_page(soup):
     header = soup.find("header")
     footer = soup.find("footer")
-
     if header:
         header.decompose()
     if footer:
@@ -18,28 +67,28 @@ def parse_page(soup: BeautifulSoup):
         .replace("CloseSearch Submit Blog", "")
     )
 
+
 @st.cache_data(show_spinner="Loading website...")
 def load_website(url):
     splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=1000,
         chunk_overlap=200,
     )
-    
     loader = SitemapLoader(
         url,
-        filter_urls=[
-            r"^(.*\/blog\/).*",
-        ],
         parsing_function=parse_page,
     )
-    loader.requests_per_second = 5
+    loader.requests_per_second = 2
     docs = loader.load_and_split(text_splitter=splitter)
-    return docs
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
+
 
 st.set_page_config(
     page_title="SiteGPT",
     page_icon="🖥️",
 )
+
 
 st.markdown(
     """
@@ -51,13 +100,24 @@ st.markdown(
 """
 )
 
+
 with st.sidebar:
-    url = st.text_input("Write down a URL", placeholder="https://example.com")
+    url = st.text_input(
+        "Write down a URL",
+        placeholder="https://example.com",
+    )
+
 
 if url:
     if ".xml" not in url:
         with st.sidebar:
             st.error("Please write down a Sitemap URL.")
     else:
-        docs = load_website(url)
-        st.write(docs)
+        retriever = load_website(url)
+
+        chain = {
+            "docs": retriever,
+            "question": RunnablePassthrough(),
+        } | RunnableLambda(get_answers)
+
+        chain.invoke("What is the pricing of GPT-4 Turbo with vision.")
